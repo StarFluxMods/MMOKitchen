a013c771bc4c29c96ecdcd5abc661cffde765dea
Lachlan Leone <starfluxgames@gmail.com>
2022-11-23 13:21:40 +1100

    Converting to VS

--- MMOKitchen/Mod.cs
+++ MMOKitchen/Mod.cs
@@ -1,29 +1,12 @@
 ﻿using KitchenLib;
 using System.Reflection;
-#if BepInEx
 using BepInEx;
-#endif
-#if MelonLoader
-using MelonLoader;
-#endif
-
-#if MelonLoader
-[assembly: MelonInfo(typeof(MMOKitchen.Mod), "MMO Kitchen", "0.1.3", "StarFluxGames")]
-[assembly: MelonGame("It's Happening", "PlateUp")]
-#endif
 namespace MMOKitchen
-{	
-    #if BepInEx
-    [BepInProcess("PlateUp.exe")]
+{
+	[BepInProcess("PlateUp.exe")]
 	[BepInPlugin("starfluxgames.mmokitchen", "MMO Kitchen", "0.1.3")]
-	#endif
-    public class Mod : BaseMod
-    {
-		#if MelonLoader
-		public Mod() : base("mmokitchen", "1.1.0") { }
-		#endif
-		#if BepInEx
-		public Mod() : base("1.1.0", Assembly.GetExecutingAssembly()) {}
-		#endif
-    }
+	public class Mod : BaseMod
+	{
+		public Mod() : base("1.1.0", Assembly.GetExecutingAssembly()) { }
+	}
 }
