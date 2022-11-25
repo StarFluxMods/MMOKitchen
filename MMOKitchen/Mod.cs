49109db6fda82e5500c7fc8b4971c52c6e0e3a17
Lachlan Leone <starfluxgames@gmail.com>
2022-11-26 00:54:35 +1100

    Fixed KitchenLib problems & Preperation for MelonLoader Support

--- MMOKitchen/Mod.cs
+++ MMOKitchen/Mod.cs
@@ -1,12 +1,30 @@
 ﻿using KitchenLib;
 using System.Reflection;
+#if BEPINEX
 using BepInEx;
+#endif
+#if MELONLOADER
+using MelonLoader;
+#endif
+
+#if MELONLOADER
+[assembly: MelonInfo(typeof(MMOKitchen.Mod), "MMO Kitchen", "0.1.4", "StarFluxGames")]
+[assembly: MelonGame("It's Happening", "PlateUp")]
+#endif
 namespace MMOKitchen
 {
+#if BEPINEX
 	[BepInProcess("PlateUp.exe")]
-	[BepInPlugin("starfluxgames.mmokitchen", "MMO Kitchen", "0.1.3")]
+	[BepInPlugin("starfluxgames.mmokitchen", "MMO Kitchen", "0.1.4")]
+	[BepInDependency(KitchenLib.Mod.GUID)]
+#endif
 	public class Mod : BaseMod
 	{
-		public Mod() : base("1.1.0", Assembly.GetExecutingAssembly()) { }
+#if MELONLOADER
+		public Mod() : base("mmokitchen", "1.1.1") { }
+#endif
+#if BEPINEX
+		public Mod() : base("1.1.1", Assembly.GetExecutingAssembly()) { }
+#endif
 	}
 }
